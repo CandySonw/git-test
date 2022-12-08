b981248e4e5170e5ddb8354b98513913b964d9dd
Administrator <1139610343@qq.com>
2022-12-08 20:48:50 +0800

    20221208-2

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -1,7 +1,8 @@
 package com.atguigu;
 
-public class gittest {
+public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git~~~");
+        System.out.println("hello git2~~~");
     }
 }
