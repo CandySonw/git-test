884ad0287f5ed03f2552ed2b64cc3e5e0c1c95ff
Administrator <1139610343@qq.com>
2022-12-08 21:11:03 +0800

    20221208 master-4

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git3~~~");
         System.out.println("hello git4~~~   by hot-fix");
         System.out.println("1-------master-----1");
+        System.out.println("1-------master-----2");
     }
 }
