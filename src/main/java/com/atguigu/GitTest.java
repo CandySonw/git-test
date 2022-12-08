dfa4513e4404236d1f8e026f558f14196253764a
CandySonw <119941984+CandySonw@users.noreply.github.com>
2022-12-08 21:31:00 +0800

    Update GitTest.java
    master6

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -10,5 +10,7 @@ public class GitTest {
         System.out.println("1-------master-----2");
         System.out.println("1------hot-fix-----1");
         System.out.println("3------master-----3");
+        System.out.println("4------master-----4    by github");
+
     }
 }
